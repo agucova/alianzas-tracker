20acad8a8f46edf04cac2045b4e0d0912082f8b0
Agustín Covarrubias <agucova@uc.cl>
2021-05-08 12:48:17 -0400

    up

--- alianzas_tracker/pbokeh.py
+++ alianzas_tracker/pbokeh.py
@@ -1,10 +1,40 @@
 from bokeh.plotting import figure, curdoc
 from bokeh.driving import linear
 from bokeh.models import HoverTool, ColumnDataSource
-from datetime import datetime
+from datetime import datetime, timedelta
 from scrape_likes import get_likes
 
 import pandas as pd
+
+def predict_3h(df):
+    if len(df["naranja"]) < 15 or len(df["verde"]) < 15:
+        return pd.DataFrame()
+    
+    i_naranja = df["naranja"].iloc[-6]
+    i_verde = df["verde"].iloc[-6]
+    f_naranja = df["naranja"].iloc[-1]
+    f_verde = df["verde"].iloc[-1]
+    f_fecha = df["tiempo"].iloc[-1]
+
+    m_naranja = (f_naranja - i_naranja)/5
+    m_verde = (f_verde - i_verde)/5
+
+    print("m naranja:", m_naranja, "m_verde:", m_verde)
+
+    delta = timedelta(minutes=1)
+    
+    prediccion = pd.DataFrame()
+    for x in range(1,181):
+        delta = timedelta(minutes=x)
+        tiempo = f_fecha + delta
+        
+        naranja = f_naranja + m_naranja*x
+        verde = f_verde + m_verde*x
+
+        fila = pd.DataFrame([[tiempo, verde, naranja]], columns=['tiempo', 'verde', 'naranja'])
+        prediccion = prediccion.append(fila, ignore_index=True)
+    
+    return prediccion
 
 
 nid = "CERyIJrDX-k"
@@ -17,6 +47,7 @@ except FileNotFoundError:
     log = pd.DataFrame(columns=columnas)
 
 source = ColumnDataSource(data=log)
+psource = ColumnDataSource(data=predict_3h(log))
 
 p = figure(x_axis_type="datetime", sizing_mode="stretch_both")
 
@@ -31,17 +62,28 @@ verde = p.line(
     name="verde",
 )
 
-sverde = p.scatter(
+# sverde = p.scatter(
+#     "tiempo",
+#     "verde",
+#     source=source,
+#     color="black",
+#     line_width=5,
+#     name="sverde",
+# )
+
+pverde = p.line(
     "tiempo",
     "verde",
-    source=source,
-    color="black",
-    line_width=5,
-    name="sverde",
+    source=psource,
+    color="green",
+    line_width=2,
+    name="pverde",
+    line_dash = "dashed"
 )
+
 p.add_tools(
     HoverTool(
-        renderers=[verde],
+        renderers=[verde, pverde],
         show_arrow=False,
         line_policy="next",
         tooltips=[
@@ -50,7 +92,7 @@ p.add_tools(
         ],
         formatters={"@tiempo": "datetime"},
         mode="vline",
-        names=["verde"],
+        names=["verde", "pverde"],
     )
 )
 naranja = p.line(
@@ -63,34 +105,32 @@ naranja = p.line(
     name="naranja",
 )
 
-snaranja = p.scatter(
+pnaranja = p.line(
     "tiempo",
     "naranja",
-    source=source,
-    color="black",
+    source=psource,
+    color="orange",
     line_width=5,
-    name="snaranja",
+    name="pnaranja",
+    line_dash = "dashed"
 )
 
-p.add_tools(
-    HoverTool(
-        renderers=[naranja],
-        show_arrow=False,
-        line_policy="next",
-        tooltips=[
-            ("Likes", "@naranja{0,0}"),
-        ],
-        formatters={"@tiempo": "datetime"},
-        mode="vline",
-        names=["naranja"],
-    )
-)
+# snaranja = p.scatter(
+#     "tiempo",
+#     "naranja",
+#     source=source,
+#     color="black",
+#     line_width=2,
+#     name="snaranja",
+# )
 
 p.legend.location = "top_left"
 p.legend.click_policy = "hide"
 
 likes_verde = verde.data_source
 likes_naranja = naranja.data_source
+plikes_verde = pverde.data_source
+plikes_naranja = pnaranja.data_source
 
 
 @linear()
@@ -114,9 +154,17 @@ def update(step):
 
     likes_verde.data = log
     likes_naranja.data = log
+    
+    prediccion = predict_3h(log)
+
+    plikes_verde.data = prediccion
+    plikes_naranja.data = prediccion
 
     likes_verde.trigger("data", likes_verde.data, likes_verde.data)
     likes_naranja.trigger("data", likes_naranja.data, likes_naranja.data)
+    plikes_verde.trigger("data", plikes_verde.data, plikes_verde.data)
+    plikes_naranja.trigger("data", plikes_naranja.data, plikes_naranja.data)
+
 
 
 curdoc().add_root(p)
--- alianzas_tracker/scrape_likes.py
+++ alianzas_tracker/scrape_likes.py
@@ -4,42 +4,26 @@ import urllib3
 import requests
 
 
-proxylist = []
+#proxylist = []
 
-with open("proxies.txt", mode="r") as proxies:
-    count = 0
-    for proxy in proxies:
-        proxylist.append(proxy.strip("\n"))
-        count += 1
-    print(count, "proxies loaded")
+# with open("proxies.txt", mode="r") as proxies:
+#    count = 0
+#    for proxy in proxies:
+#        proxylist.append(proxy.strip("\n"))
+#        count += 1
+#    print(count, "proxies loaded")
 
 ig = Instagram()
 
 
 def get_likes(code):
-    proxies = {}
-    proxy = choice(proxylist)
-    proxies["http"] = proxy
-    proxies["https"] = proxy
+#    proxies = {}
+#    proxy = choice(proxylist)
+#    proxies["http"] = proxy
+#    proxies["https"] = proxy
     # ig.set_proxies(proxies)
 
     post = None
     while post is None:
-        try:
-            post = ig.get_medias_by_code(code)
-        except urllib3.exceptions.ProxySchemeUnknown:
-            print("Bad proxy, retrying.")
-            proxylist.remove(proxy)
-            proxy = choice(proxylist)
-            proxies["http"] = proxy
-            proxies["https"] = proxy
-            ig.set_proxies(proxies)
-        except (requests.exceptions.ProxyError, requests.exceptions.ConnectionError):
-            print("Dead proxy, retrying.")
-            proxylist.remove(proxy)
-            proxy = choice(proxylist)
-            proxies["http"] = proxy
-            proxies["https"] = proxy
-            ig.set_proxies(proxies)
-
+        post = ig.get_medias_by_code(code)
     return int(post.likes_count)
